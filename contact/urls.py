49fdb2f2279a894fd80cb548d6ac0952ce37fe17
Gustavo Marques <gustavo-markes@hotmail.com>
2025-04-04 21:57:57 -0300

    finalizando a exclusão de contatos

--- contact/urls.py
+++ contact/urls.py
@@ -4,8 +4,10 @@ from contact import views
 app_name = "contact"
 
 urlpatterns = [
+    path("", views.index, name="index"),
+    path("search/", views.search, name="search"),
     path("contact/<int:contact_id>/", views.contact, name="contact"),
     path("contact/create/", views.create, name="create"),
-    path("search/", views.search, name="search"),
-    path("", views.index, name="index"),
+    path("contact/<int:contact_id>/update", views.update, name="update"),
+    path("contact/<int:contact_id>/delete", views.delete, name="delete"),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render,  get_object_or_404, redirect
-from django.http import Http404
 from django.db.models import Q
 from django.core.paginator import Paginator
 
